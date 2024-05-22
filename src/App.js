a4dea4059d638e8d706d276164a31493ae07ba20
Asangabia Mole <moleasangabia23@gmail.com>
2024-05-22 13:23:42 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Flashcard from "./Flashcard.js";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Flashcard />
+      </div>
+    </>
+  );
+}
+
+export default App;
